17a8a279c3c5328fb478e8e7c03b710265924774
Junya Takaesu <takaesu_junya@ca-adv.co.jp>
2024-06-08 00:13:26 +0900

    Add Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/img/logo.jpg";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 const Navbar = () => {
-    return <div>Navbar</div>;
-}
- 
+  return (
+    <div className="bg-primary dark:bg-slate-700 py-2 px-5 flex justify-between ">
+      <Link href="/">
+        <Image
+          src={logo}
+          alt="junyapress"
+          width={40}
+          className="rounded-full"
+        />
+      </Link>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="focus:outline-none">
+          <Avatar>
+            <AvatarImage
+              src="https://avatars.githubusercontent.com/u/84374573?v=4"
+              alt="@takaesu-junya"
+            />
+            <AvatarFallback className="text-black">JT</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <Link href="/profile">Profile</Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Link href="/auth">Logout</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
 export default Navbar;
